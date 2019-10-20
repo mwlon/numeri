588df1f18cd29d529fbb94a5ddc484f2d2becb99
mwlon <m.w.loncaric@gmail.com>
2019-10-20 09:49:26 -0400

    use eval for getIByJ too

--- speed_test/tests/symEig.js
+++ speed_test/tests/symEig.js
@@ -5,7 +5,7 @@ const n = 100;
 const randMat = numeri.empty([n, n]).mapInPlace(() => Math.random());
 const symMat = randMat.plus(randMat.transpose());
 
-time(() => numeri.symHessenberg(symMat, {includeQ: false}), 'warmup', 170);
+time(() => numeri.symHessenberg(symMat, {includeQ: false}), 'eig_warmup', 170);
 time(() => numeri.symHessenberg(symMat, {includeQ: false}), 'symHessenberg without q', 92);
 time(() => numeri.symHessenberg(symMat, {includeQ: true}), 'symHessenberg with q', 152);
 time(() => numeri.symEig(symMat, {includeVecs: false}), 'symEig without vecs', 109);
