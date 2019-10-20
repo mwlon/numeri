3595b3bca326df7ee002453823084dabcb75c529
mwlon <m.w.loncaric@gmail.com>
2019-10-20 00:29:20 -0400

    get i via eval

--- speed_test/speedTestUtils.js
+++ speed_test/speedTestUtils.js
@@ -7,16 +7,17 @@ module.exports = {
 
     let color;
     if (ratio < 1.2) {
-      color = '\x1b[32m';
+      color = '\x1b[32m'; //green
     } else if (ratio < 1.5) {
-      color = '\x1b[33m';
+      color = '\x1b[33m'; //yellow
     } else {
-      color = '\x1b[31m';
+      color = '\x1b[31m'; //red
     }
 
     console.log(`${name} step ${color}[${new Date() - t} vs ${expected} ms]\x1b[0m`);
     return res;
   },
+  
   time: (op, name, expected) => {
     module.exports.timeAndReturn(op, name, expected);
   },
---
+++ speed_test/tests/get.js
@@ -0,0 +1,19 @@
+const numeri = require('../../lib/index.js');
+const { time } = require('../speedTestUtils');
+
+const highD = numeri.fill([5, 5, 5, 5, 5], 1);
+
+time(() => {
+  let s = 0;
+  for (var iter = 0; iter < 10000000; iter++) {
+    s += highD.get(iter % 2, 0, 1, 2, 3);
+  }
+}, 'get_high_dimensions', 25);
+
+const matrix = numeri.fill([5, 5], 2);
+time(() => {
+  let s = 0;
+  for (var iter = 0; iter < 10000000; iter++) {
+    s += matrix.get(1, 0);
+  }
+}, 'get_matrix', 14);
