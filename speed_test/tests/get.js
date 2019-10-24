7e50bc4c98bd3b2119527b56adc56c2158f14780
mwlon <m.w.loncaric@gmail.com>
2019-10-23 21:11:19 -0400

    reset get speed test

--- speed_test/tests/get.js
+++ speed_test/tests/get.js
@@ -5,7 +5,7 @@ const highD = numeri.fill([5, 5, 5, 5, 5], 1);
 
 time(() => {
   let s = 0;
-  for (var iter = 0; iter < 100000000; iter++) {
+  for (var iter = 0; iter < 10000000; iter++) {
     s += highD.get(iter % 2, 0, 1, 2, 3);
   }
 }, 'get_high_dimensions', 25);
@@ -13,7 +13,7 @@ time(() => {
 const matrix = numeri.fill([5, 5], 2);
 time(() => {
   let s = 0;
-  for (var iter = 0; iter < 100000000; iter++) {
+  for (var iter = 0; iter < 10000000; iter++) {
     s += matrix.get(1, 0);
   }
 }, 'get_matrix', 14);
