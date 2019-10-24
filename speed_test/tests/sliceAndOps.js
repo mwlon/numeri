22eceec5533ae4e8f3fedb41fc3d5d55206760af
mwlon <m.w.loncaric@gmail.com>
2019-10-23 21:17:30 -0400

    save speed improvements

--- speed_test/tests/sliceAndOps.js
+++ speed_test/tests/sliceAndOps.js
@@ -5,5 +5,5 @@ const ones = numeri.fill([3000, 4000], 1);
 
 const sliced = timeAndReturn(() => ones.slice(undefined, [500, 3500]), 'slice', 1);
 const transposed = timeAndReturn(() => sliced.transpose(), 'transpose', 1);
-time(() => transposed.copy(), 'copy', 440);
-time(() => transposed.plus(transposed), 'complicated_add', 698);
+time(() => transposed.copy(), 'copy', 184);
+time(() => transposed.plus(transposed), 'complicated_add', 220);
