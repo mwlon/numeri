b7f34ec103597b1b1dcc6d2b7e227d760b9a6700
mwlon <m.w.loncaric@gmail.com>
2019-10-26 23:32:41 -0400

    effectively use function cache

--- speed_test/tests/matMulTransposed.js
+++ speed_test/tests/matMulTransposed.js
@@ -2,4 +2,4 @@ const numeri = require('../../lib/index.js');
 const { time } = require('../speedTestUtils');
 
 const small = numeri.fill([300, 300], 1);
-time(() => numeri.matMul(small, small), 'mat_mul', 100);
+time(() => numeri.matMul(small, small.transpose()), 'mat_mul_transposed', 75);
